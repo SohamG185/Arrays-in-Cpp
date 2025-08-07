8e79e631819eeb6b1b7dad73303f461955b8fd1e
SohamG185 <sohammg13@gmail.com>
2025-08-07 15:03:32 +0530

    Create Declare-and-initialize-strings.cpp

---
+++ Declare-and-initialize-strings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string str1 = "Hello, World!";
+    cout << "String 1: " << str1 << endl;
+
+    string str2;
+    str2 = "Hello from C++!";
+    cout << "String 2: " << str2 << endl;
+
+    char str3[] = "Hello, Array!";
+    cout << "String 3: " << str3 << endl;
+
+    return 0;
+}
